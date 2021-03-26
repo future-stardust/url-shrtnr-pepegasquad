a7b57c357fc6a99ab72c42e930cdc5a211ca59ef
Annibezh <anytkin11@gmail.com>
2021-03-26 20:39:21 +0200

    Does created alias exists property based test

---
+++ src/test/java/edu/kpi/testcourse/propertybased/LogicTestPropertyBased.java
@@ -0,0 +1,27 @@
+package edu.kpi.testcourse.propertybased;
+
+import org.junit.jupiter.api.Test;
+import static org.quicktheories.QuickTheory.qt;
+import static org.quicktheories.generators.SourceDSL.strings;
+import edu.kpi.testcourse.logic.UserActions;
+
+public class LogicTestPropertyBased {
+
+  static final int MIN_LENGTH = 6;
+  static final int MAX_LENGTH = 10;
+
+  UserActions userActions = new UserActions();
+
+  @Test
+  void doesCreatedAliasExist_propertyBased() {
+    qt().forAll(
+      strings().basicLatinAlphabet().ofLengthBetween(MIN_LENGTH, MAX_LENGTH),
+      strings().basicLatinAlphabet().ofLengthBetween(MIN_LENGTH, MAX_LENGTH)
+    ).check((alias, url) -> {
+      // WHEN
+      userActions.putUrl(alias, url);
+      // THEN
+      return userActions.retrieveUrl(alias)==url;
+    });
+  }
+}
